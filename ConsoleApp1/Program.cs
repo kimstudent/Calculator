dbb8a51070e13a36b64b86d5a09695e3bed98c15
LocalGhost <kimyana081004@gmail.com>
2024-09-02 12:55:14 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Первое число");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Второе число");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Выберите (+, -, *, /):");
+        string operation = Console.ReadLine();
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("НОЛЬ");
+                    return;
+                }
+                break;
+            default:
+                Console.WriteLine("ОШИБКА");
+                return;
+        }
+
+        Console.WriteLine($"Результат: {result}");
+    
+        Console.ReadKey();
+    }
+
+}
