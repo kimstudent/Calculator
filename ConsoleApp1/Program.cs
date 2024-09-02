66e0eee0acac99637218791f783d4a649384a3b3
kimstudent <132489828+kimstudent@users.noreply.github.com>
2024-09-02 14:27:32 +0500

    Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,49 +1,62 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Первое число");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Второе число");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+        bool Continue = true;
 
-        Console.WriteLine("Выберите (+, -, *, /):");
-        string operation = Console.ReadLine();
-
-        double result = 0;
-
-        switch (operation)
+        while (Continue)
         {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                else
-                {
-                    Console.WriteLine("НОЛЬ");
-                    return;
-                }
-                break;
-            default:
-                Console.WriteLine("ОШИБКА");
-                return;
+            Console.WriteLine("Первое число");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Второе число");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Выберите (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            bool a = true;
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("НОЛЬ");
+                        a = false;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("ОШИБКА");
+                    a = false;
+                    break;
+            }
+            if (a)
+            {
+                Console.WriteLine($"Результат: {result}");
+            }
+            Console.WriteLine("Продолжить?(да/нет)");
+            string Response = Console.ReadLine();
+            if (Response != "да")
+            {
+                Continue = false;
+            }
         }
-
-        Console.WriteLine($"Результат: {result}");
-    
-        Console.ReadKey();
+        Console.WriteLine("Конец");
     }
-
 }
